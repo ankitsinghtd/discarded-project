a37b03675094568188ab4c18f4219203d39a3f97
Ankit Singh <ankitshekhawat46931@gmail.com>
2024-02-17 20:17:04 +0530

    sign in feature

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,97 @@
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_AUTHN_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+const signInButton = document.getElementById("signInButton");
+const signOutButton = document.getElementById("signOutButton");
+const message = document.getElementById("message");
+const emailPasswordForm = document.getElementById("emailPasswordForm");
+
+signOutButton.style.display = "none";
+message.style.display = "none";
+
+const userSignInWithGoogle = async () => {
+  firebase
+    .auth()
+    .signInWithPopup(googleProvider)
+    .then((result) => {
+      const user = result.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+};
+
+const userSignInWithEmailPassword = async (e) => {
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    });
+};
+
+const userSignOut = async () => {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      alert("You have signed out successfully!");
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+};
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    signOutButton.style.display = "block";
+    message.style.display = "block";
+    userName.innerHTML = user.displayName;
+    userEmail.innerHTML = user.email;
+  } else {
+    signOutButton.style.display = "none";
+    message.style.display = "none";
+  }
+});
+
+signInButton.addEventListener("click", userSignInWithGoogle);
+emailPasswordForm.addEventListener("submit", userSignInWithEmailPassword);
+signOutButton.addEventListener("click", userSignOut);
+
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
